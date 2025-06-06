6325aa2e30c4198ac8c75cc9f85392e34f0d9f57
Andreas Thielmann <thielmann.andreas@googlemail.com>
2025-06-06 14:52:00 +0200

    add files

---
+++ markdown_document_builder.py
@@ -0,0 +1,31 @@
+from markdown_document import *
+
+class MarkdownDocumentBuilder:
+    def __init__(self) -> None:
+        self.document = MarkdownDocument()
+
+    def build(self):
+        return self.document
+
+    def withSection(self, text : str, level : int):
+        self.document.append(MarkdownSection(text, level, lineNumber=-1))
+        return self
+
+    def withTable(self, table : MarkdownTable):
+        self.document.append(table)
+        return self
+
+class MarkdownTableBuilder:
+    def __init__(self) -> None:
+        self.table = MarkdownTable(MarkdownTableRow([], -1))
+
+    def build(self):
+        return self.table
+
+    def withHeader(self, *args):
+        self.table._headerRow = MarkdownTableRow(list(args), -1)
+        return self
+
+    def withRow(self, *args):
+        self.table.rows.append(MarkdownTableRow(list(args), -1))
+        return self
---
+++ markdown_parser.py
@@ -0,0 +1,69 @@
+import re
+from markdown_document import *
+
+class TableRowException(Exception):
+    def __init__(self, lineNumber : int, *args: object) -> None:
+        super().__init__(*args)
+        self.message = f"No matching table header in line {lineNumber}"
+
+class MarkdownParser:
+    def __init__(self) -> None:
+        self._tableRowRegex = re.compile(r"\|(.+\|)+")
+        self._tableSeparatorRegex = re.compile(r"\|([\s-]+\|)+")
+        self._state = self._parsingDocumentState
+
+    def parse(self, input : str) -> MarkdownDocument:
+        self._document = MarkdownDocument()
+        self._currentTableItem = None
+
+        lines = input.splitlines()
+
+        previousLine = None
+
+        for lineIdx, line in enumerate(lines):
+            lineNumber = lineIdx + 1
+            self._state(line, lineNumber, previousLine)
+            previousLine = line
+
+        if self._currentTableItem != None:
+            self._document.append(self._currentTableItem)
+
+        return self._document
+
+    def _parsingDocumentState(self, line : str, lineNumber : int, previousLine : str):
+        if self._isSection(line):
+            self._document.append(self._createSection(line, lineNumber))
+        elif self._isTableSeparator(line):
+            self._currentTableItem = MarkdownTable(self._parseTableRow(previousLine, lineNumber - 1))
+            self._state = self._parsingTableState
+
+    def _parsingTableState(self, line : str, lineNumber : int, previousLine : str):
+        if not line:
+            self._document.append(self._currentTableItem)
+            self._currentTableItem = None
+            self._state = self._parsingDocumentState
+        else:
+            self._currentTableItem.rows.append(self._parseTableRow(line, lineNumber))
+
+    def _isSection(self, line : str) -> bool:
+        return line.startswith('#')
+
+    def _isTableRow(self, line : str) -> bool:
+        return self._tableRowRegex.match(line) != None
+
+    def _parseTableRow(self, line : str, lineNumber : int) -> list:
+        strippedLine = line.strip()
+
+        if not self._isTableRow(strippedLine):
+            raise TableRowException(lineNumber)
+
+        rawEntries = strippedLine.strip("|").split("|")
+        strippedEntries = [e.strip() for e in rawEntries]
+        return MarkdownTableRow(strippedEntries, lineNumber)
+
+    def _isTableSeparator(self, line : str) -> bool:
+        return self._tableSeparatorRegex.match(line) != None
+
+    def _createSection(self, line : str, lineNumber : int):
+        dummyLevel = 0
+        return MarkdownSection(line.strip().strip("#").strip(), dummyLevel, lineNumber)
